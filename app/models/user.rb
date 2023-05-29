ac8887e2c2a49b7ef0a803ee34afa0503e04c0db
knskymsk <knsk.smnc.30@gmail.com>
2023-05-29 23:59:47 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,              presence: true
+  validates :family_name,           presence: true
+  validates :first_name,            presence: true
+  validates :family_name_katakana,  presence: true
+  validates :first_name_katakana,   presence: true
+  validates :birthday,              presence: true
+
+  has_many :items
+
+end
