d1bb894a2afdfc05c5ad5508584c952a67df2568
knskymsk <knsk.smnc.30@gmail.com>
2023-05-31 20:15:49 +0900

    バリデーション　修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   validates :family_name_katakana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_katakana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday,              presence: true
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
+  validates :password, format: { with: /\A[a-z0-9]+\z/}
 
 end
