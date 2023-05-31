4a6ae9f3f2c12dda2f4bc3e1d31294cbebd63a6a
knskymsk <knsk.smnc.30@gmail.com>
2023-05-31 19:24:00 +0900

    バリデーション　変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,              presence: true
-  validates :family_name,           presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
-  validates :first_name,            presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :family_name,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :family_name_katakana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_katakana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday,              presence: true
