3399074d3abef8573ea3d27c471e7a2b55fd90e7
knskymsk <knsk.smnc.30@gmail.com>
2023-06-03 17:39:58 +0900

    不要なコメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items, only: [:new, :create, :index]
-  resources :users, only: [:show, :index, :new]
-    #resources :items, only: [:new, :create, :show,]
-  #end
+
 end
 
