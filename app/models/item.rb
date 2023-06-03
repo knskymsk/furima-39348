9c2f25a357ba1b7d281897726060441cf8cf4d7c
knskymsk <knsk.smnc.30@gmail.com>
2023-06-03 17:36:53 +0900

    only_integer: true 記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   validates :name,                presence: true
   validates :description,         presence: true
-  validates :price,               presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,               presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :condition_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_charge_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :area_id,             presence: true, numericality: { other_than: 1 , message: "can't be blank"}
