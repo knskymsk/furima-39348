4dd361ea2d7e2aa159f1751c2db063616245e0e2
knskymsk <knsk.smnc.30@gmail.com>
2023-06-07 21:07:40 +0900

    テストコード

--- app/models/management_purchase.rb
+++ app/models/management_purchase.rb
@@ -3,12 +3,12 @@ class ManagementPurchase
   attr_accessor :zip_code, :area_id, :city, :address, :building_name, :phone_number, :management_id, :user_id, :item_id
 
   with_options presence: true do
-    validates :zip_code
-    validates :area_id
+    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
-    validates :building_name
-    validates :phone_number
+    #validates :building_name
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
     validates :user_id
     validates :item_id
   end
---
+++ spec/factories/management_purchases.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :management_purchase do
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    zip_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    area_id { Faker::Number.between(from: 1, to: 47) }
+    city { Faker::Address.city }
+    address { Faker::Address.street_address }
+    building_name { Faker::Address.street_address }
+    phone_number { Faker::Number.decimal_part(digits: 11) }
+  end
+end
