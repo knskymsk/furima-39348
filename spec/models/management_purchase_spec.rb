7ca654ee16d02c9a50c8c4c2e447ccbb78d484e0
knskymsk <knsk.smnc.30@gmail.com>
2023-06-08 21:39:29 +0900

    修正

--- spec/models/management_purchase_spec.rb
+++ spec/models/management_purchase_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ManagementPurchase, type: :model do
   before do
-    @management_purchase = FactoryBot.build(:management_purchase)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @management_purchase = FactoryBot.build(:management_purchase,user_id: @user.id, item_id: @item.id)
   end
 
   describe '配送先情報の保存' do
