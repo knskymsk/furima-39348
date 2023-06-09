c066cd408b17ef0074e997e083191dd652676068
knskymsk <knsk.smnc.30@gmail.com>
2023-06-09 19:17:45 +0900

    修正

--- app/models/management_purchase.rb
+++ app/models/management_purchase.rb
@@ -4,7 +4,7 @@ class ManagementPurchase
 
   with_options presence: true do
     validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     #validates :building_name
--- spec/factories/management_purchases.rb
+++ spec/factories/management_purchases.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     # user_id { Faker::Number.non_zero_digit }
     # item_id { Faker::Number.non_zero_digit }
     zip_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
-    area_id { Faker::Number.between(from: 1, to: 47) }
+    # area_id { Faker::Number.between(from: 1, to: 47) }
+    area_id {2}
     city { Faker::Address.city }
     address { Faker::Address.street_address }
     building_name { Faker::Address.street_address }
