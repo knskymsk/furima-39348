c5913fdc843860f44f0736c530f0e401930c11fe
knskymsk <knsk.smnc.30@gmail.com>
2023-06-09 20:11:17 +0900

    修正2

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -23,11 +23,6 @@ class ManagementsController < ApplicationController
     params.require(:management_purchase).permit(:zip_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def non_purchase_item
-    @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id || @item.management.present?
-  end
-
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
@@ -35,6 +30,11 @@ class ManagementsController < ApplicationController
       card: management_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def non_purchase_item
+    @item = Item.find(params[:item_id])
+    redirect_to user_session_path if @item.user_id || @item.management.present?
   end
 
 end
